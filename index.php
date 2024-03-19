2dac0c76a1d8c2631f7a6b78c827e7e615e5d0af
Andrey Harutyunyan <and250988@mail.ru>
2024-03-19 15:23:45 +0400

    Clean up codes

--- index.php
+++ index.php
@@ -4,10 +4,5 @@ require 'vendor/autoload.php';
 
 $uri = $_SERVER['REQUEST_URI'];
 
-//$router = require_once 'core/helpers.php';
-//$t1 = config('ddd');
-//echo '<pre>';var_dump($t1);
-//$t2 = config('zzz');
-//echo '<pre>';var_dump($t2);
 $router = require_once 'app/routes.php';
 $router->handle($uri);
